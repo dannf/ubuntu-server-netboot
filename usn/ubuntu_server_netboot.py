6be8025d9edf6c303c38cac66537eb50b0daa58e
Taihsiang Ho (tai271828) <taihsiang.ho@canonical.com>
2021-05-04 15:15:08 +0200

    feat(poetry): init the python package with poetry

--- usn/ubuntu_server_netboot.py
+++ usn/ubuntu_server_netboot.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 # Copyright (C) 2020 Canonical, Ltd.
 #
 # This program is free software; you can redistribute it and/or
@@ -16,19 +14,19 @@
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
-import argparse
-import atexit
 import distro_info
-import logging
 import os
 import re
 import shutil
 import subprocess
-import sys
-import tempfile
 import urllib.request
 
+
 Netboot_Args = ["root=/dev/ram0", "ramdisk_size=1500000", "ip=dhcp"]
+Ubuntu_Arch_to_Uefi_Arch_Abbrev = {
+    "amd64": "x64",
+    "arm64": "aa64",
+}
 
 
 class UbuntuDistroInfoWithVersionSupport(distro_info.UbuntuDistroInfo):
@@ -166,13 +164,7 @@ def select_mirror(arch):
         return "http://ports.ubuntu.com/ubuntu-ports"
 
 
-Ubuntu_Arch_to_Uefi_Arch_Abbrev = {
-    "amd64": "x64",
-    "arm64": "aa64",
-}
-
-
-def download_bootnet(release, architecture, destdir):
+def download_bootnet(release, architecture, destdir, logger):
     uefi_arch_abbrev = Ubuntu_Arch_to_Uefi_Arch_Abbrev[architecture]
     for pocket in ["%s-updates" % (release), release]:
         url = "%s/dists/%s/main/uefi/grub2-%s/current/grubnet%s.efi.signed" % (
@@ -194,7 +186,7 @@ def download_bootnet(release, architecture, destdir):
     raise Exception("Could not download %s" % (url))
 
 
-def download_pxelinux(release, destdir):
+def download_pxelinux(release, destdir, logger):
     for pocket in ["%s-updates" % (release), release]:
         mirror = select_mirror("amd64")
         url = (
@@ -214,134 +206,21 @@ def download_pxelinux(release, destdir):
     raise Exception("Could not download %s" % (url))
 
 
-def setup_kernel_params(bootloader_cfg):
+def setup_kernel_params(bootloader_cfg, url, autoinstall_url, extra_args):
     bootloader_cfg.add_kernel_params(
-        Netboot_Args + ["url=%s" % (args.url)], install_only=True
+        Netboot_Args + ["url=%s" % (url)], install_only=True
     )
-    if args.autoinstall_url:
+    if autoinstall_url:
         bootloader_cfg.add_kernel_params(
             [
-                'autoinstall "ds=nocloud-net;s=%s"' % (args.autoinstall_url),
+                'autoinstall "ds=nocloud-net;s=%s"' % (autoinstall_url),
             ],
             install_only=True,
         )
-    if args.extra_args:
-        bootloader_cfg.add_kernel_params(args.extra_args.split(" "))
+    if extra_args:
+        bootloader_cfg.add_kernel_params(extra_args.split(" "))
 
 
-def cleanup(directory):
+def cleanup(directory, logger):
     logger.info("Cleaning up %s" % (directory))
     shutil.rmtree(directory)
-
-
-if __name__ == "__main__":
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s: %(message)s")
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    parser = argparse.ArgumentParser(
-        description="Generate a netboot tree from an Ubuntu Server live ISO"
-    )
-    parser.add_argument(
-        "-e",
-        "--extra-args",
-        help="Any additional kernel command line arguments",
-    )
-    parser.add_argument(
-        "--iso",
-        help="Local copy of Server Live ISO"
-        + " (--url should point to a copy of the same file)",
-    )
-    parser.add_argument(
-        "-o",
-        "--out-dir",
-        help="Output directory",
-    )
-    parser.add_argument(
-        "--url",
-        help="URL to Server Live ISO to be downloaded at install-time",
-        required=True,
-    )
-    parser.add_argument(
-        "--autoinstall-url",
-        help="URL to Autoinstall config file to be used during Subiquity installation",
-    )
-
-    args = parser.parse_args()
-    if args.iso:
-        iso = ServerLiveIso(args.iso)
-    else:
-        logger.info("Downloading %s" % (args.url))
-        with urllib.request.urlopen(args.url) as response:
-            with tempfile.NamedTemporaryFile(delete=False) as iso:
-                atexit.register(os.remove, iso.name)
-                shutil.copyfileobj(response, iso)
-                iso = ServerLiveIso(iso.name)
-
-    architecture = iso.architecture
-    release = iso.codename
-
-    staging_root = args.out_dir or tempfile.mkdtemp()
-    staging_dir = os.path.join(staging_root, "ubuntu-installer")
-    os.mkdir(staging_dir)
-    if args.out_dir:
-        atexit.register(cleanup, staging_dir)
-    else:
-        atexit.register(cleanup, staging_root)
-
-    download_bootnet(release, architecture, staging_dir)
-
-    os.mkdir(os.path.join(staging_dir, "casper"))
-    for f in ["vmlinuz", "initrd"]:
-        iso.extract_file(
-            os.path.join(os.sep, "casper", f),
-            os.path.join(staging_dir, "casper", f),
-        )
-    try:
-        for hwe_f in ["hwe-vmlinuz", "hwe-initrd"]:
-            iso.extract_file(
-                os.path.join(os.sep, "casper", hwe_f),
-                os.path.join(staging_dir, "casper", hwe_f),
-            )
-    except FileNotFoundError:
-        logger.info("No HWE boot files found, skipping")
-        pass
-
-    grub_cfg_orig = iso.read_file(os.path.join(os.sep, "boot", "grub", "grub.cfg"))
-    grub_cfg = GrubConfig(grub_cfg_orig.decode("utf-8"))
-    setup_kernel_params(grub_cfg)
-
-    os.mkdir(os.path.join(staging_dir, "grub"))
-    with open(os.path.join(staging_dir, "grub", "grub.cfg"), "w") as grub_f:
-        grub_f.write(str(grub_cfg))
-
-    if architecture == "amd64":
-        local_files = [
-            (os.path.join(os.sep, "usr", "lib", "PXELINUX", "pxelinux.0"), "pxelinux"),
-            (
-                os.path.join(
-                    os.sep, "usr", "lib", "syslinux", "modules", "bios", "ldlinux.c32"
-                ),
-                "syslinux-common",
-            ),
-        ]
-        for (local_file, pkg) in local_files:
-            try:
-                shutil.copy(local_file, staging_dir)
-            except FileNotFoundError as err:
-                sys.stderr.write("%s\n" % (err))
-                sys.stderr.write("Try installing %s.\n" % (pkg))
-                sys.exit(1)
-
-        pxelinux_dir = os.path.join(staging_dir, "pxelinux.cfg")
-        os.mkdir(pxelinux_dir)
-        pxelinux_cfg = PxelinuxConfig()
-        setup_kernel_params(pxelinux_cfg)
-        with open(os.path.join(pxelinux_dir, "default"), "w") as pxelinux_f:
-            pxelinux_f.write(str(pxelinux_cfg))
-
-    atexit.unregister(cleanup)
-    logger.info("Netboot generation complete: %s" % (staging_dir))
